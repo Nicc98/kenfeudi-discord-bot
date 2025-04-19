52f1f33a8a6642314f801e6b6823370dcbef55b3
Niks Veipāns <niksv98@gmail.com>
2025-04-19 12:44:54 +0300

    enable message and guild intents

--- bot.py
+++ bot.py
@@ -9,6 +9,8 @@ if TOKEN is None:
     raise ValueError("No token provided. Please set the DISCORD_TOKEN env variable in '.env' file.")
 
 intents = discord.Intents.default()
+intents.message_content = True # Enable message content intents
+intents.guilds = True # Enable guild intents
 client = discord.Client(intents=intents)
 
 @client.event
