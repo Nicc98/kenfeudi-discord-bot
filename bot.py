d3a83980f3429ac1f04cb17c16849dd8deacbd91
Niks Veipāns <niksv98@gmail.com>
2025-04-19 12:41:59 +0300

    add waking up the discord bot

---
+++ bot.py
@@ -0,0 +1,18 @@
+# Python Discord Bot
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv() # Load environment variables from .env file
+TOKEN = os.getenv('DISCORD_TOKEN')
+if TOKEN is None:
+    raise ValueError("No token provided. Please set the DISCORD_TOKEN env variable in '.env' file.")
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
