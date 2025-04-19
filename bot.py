e003c3067884048bbaf18f6e933892798c46413b
Niks Veipāns <niksv98@gmail.com>
2025-04-19 13:29:12 +0300

    add template for on message action

--- bot.py
+++ bot.py
@@ -2,11 +2,15 @@
 import os
 import discord
 from dotenv import load_dotenv
+from bot_helpers import *
 
-load_dotenv() # Load environment variables from .env file
+# Load environment variables from .env file
+load_dotenv() 
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 if TOKEN is None:
-    raise ValueError("No token provided. Please set the DISCORD_TOKEN env variable in '.env' file.")
+    raise ValueError("No token provided." \
+                     "Please set the DISCORD_TOKEN env variable in '.env' file.")
 
 intents = discord.Intents.default()
 intents.message_content = True # Enable message content intents
@@ -15,6 +19,21 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    guild = discord.utils.get(client.guilds, name=GUILD)
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return # Ignore messages from the bot itself
+        
+    
+    if message.content == '!mc':
+        response = "Test response!"
+        await message.channel.send(response)
 
 client.run(TOKEN)
